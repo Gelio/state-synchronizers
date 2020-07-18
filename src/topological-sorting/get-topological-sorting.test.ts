38ec8ca9985cb912253dc1639f8e03c90c43c512
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-07-18 21:44:51 +0200

    test: add tests for getTopologicalSorting

---
+++ src/topological-sorting/get-topological-sorting.test.ts
@@ -0,0 +1,56 @@
+import { getTopologicalSorting } from './get-topological-sorting';
+
+describe('getTopologicalSorting', () => {
+  interface TopologicalSortingScenario {
+    description: string;
+    edges: Record<string, string[]>;
+    expectedResult: string[];
+  }
+
+  const topologicalSortingScenarios: TopologicalSortingScenario[] = [
+    {
+      description: 'simple 3 vertex graph',
+      edges: { a: ['b', 'c'], b: ['c'] },
+      expectedResult: ['a', 'b', 'c'],
+    },
+    {
+      description: 'long chain',
+      edges: { a: ['b'], b: ['c'], c: ['d'], d: ['e'] },
+      expectedResult: ['a', 'b', 'c', 'd', 'e'],
+    },
+  ];
+
+  describe.each(topologicalSortingScenarios)(
+    'should return a topological sorting for',
+    ({ edges, expectedResult, description }) => {
+      // eslint-disable-next-line jest/valid-title
+      it(description, () => {
+        expect.hasAssertions();
+
+        const result = getTopologicalSorting(edges);
+
+        expect(result).toStrictEqual(expectedResult);
+      });
+    },
+  );
+
+  it('should return a topological sorting for two connected components', () => {
+    expect.hasAssertions();
+
+    const edges = {
+      a: ['b'],
+      b: ['c'],
+
+      foo: ['bar'],
+      bar: ['baz'],
+    };
+
+    const result = getTopologicalSorting(edges);
+
+    expect(result.indexOf('a')).toBeLessThan(result.indexOf('b'));
+    expect(result.indexOf('b')).toBeLessThan(result.indexOf('c'));
+
+    expect(result.indexOf('foo')).toBeLessThan(result.indexOf('bar'));
+    expect(result.indexOf('bar')).toBeLessThan(result.indexOf('baz'));
+  });
+});
