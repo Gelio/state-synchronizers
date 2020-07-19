9949128d25bd2f63290aee7f82ea50112072c513
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-07-19 10:12:52 +0200

    refactor: shorten type constraint for state keys

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,7 @@ export type StateUpdater<S> = (state: S) => S;
 
 export type StateSynchronizer<S> = (state: S, previousState: Readonly<S>) => S;
 
-export interface ComposableStateSynchronizer<
-  S,
-  K extends string | symbol | number = keyof S
-> {
+export interface ComposableStateSynchronizer<S, K extends keyof any = keyof S> {
   stateKey: K;
   dependenciesKeys: K[];
   synchronizer: StateSynchronizer<S>;
